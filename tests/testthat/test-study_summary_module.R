596dee32a772a4e4fe235146390aa26e069717cf
andrewelamb <andrewelamb@gmail.com>
2022-04-06 11:01:06 -0700

    fix tests after updating rds/json files

--- tests/testthat/test-study_summary_module.R
+++ tests/testthat/test-study_summary_module.R
@@ -45,38 +45,6 @@ test_that("nf_study_summary_module_server", {
   )
 })
 
-test_that("nf_gff_study_summary_module_server", {
-  shiny::testServer(
-    study_summary_module_server,
-    args = list(
-      "data" = shiny::reactive(nf_gff_data),
-      "config" = shiny::reactive(get_nf_study_summary_config())
-    ),
-    {
-      session$setInputs("study_summary-filter_value" = "All")
-      session$setInputs("study_summary-study_table_rows_selected" = 3)
-
-      expect_type(output$header_text, "character")
-
-      expect_type(filtered_data(), "list")
-      expect_named(filtered_data(), c('tables', 'selected_group', 'selected_study'))
-      expect_type(filtered_data()$tables, "list")
-      expect_named(filtered_data()$tables, c('files', 'publications', 'studies', 'tools', 'incoming_data'))
-      expect_equal(filtered_data()$selected_group, "GFF")
-      expect_equal(filtered_data()$selected_study, "Ataluren-promoted therapeutic nonsense suppression for neurofibromatosis")
-      expect_true(nrow(filtered_data()$tables$incoming_data) == 0)
-
-      expect_type(output$study_summary, "character")
-      expect_error(
-        output$data_focus_plot,
-        "The investigators have not uploaded data for this study yet. Please check back later."
-      )
-
-    }
-  )
-})
-
-
 test_that("csbc_study_summary_module_server", {
   shiny::testServer(
     study_summary_module_server,
